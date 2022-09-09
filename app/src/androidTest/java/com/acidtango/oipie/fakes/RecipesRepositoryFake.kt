aa9ad75228509166cdea1b9d34cf00fa4876a605
Abraham Cárdenes <abraham@acidtango.com>
2022-09-09 12:48:49 +0100

    fix: ui test

--- app/src/androidTest/java/com/acidtango/oipie/fakes/RecipesRepositoryFake.kt
+++ app/src/androidTest/java/com/acidtango/oipie/fakes/RecipesRepositoryFake.kt
@@ -15,24 +15,26 @@ class RecipesRepositoryFake
 constructor() : ReceiptRepository {
     override suspend fun getAllReceipts(offset: Int, limit: Int): Flow<Resource<Receipts>> {
         return flow {
-            Resource.Success(
-                data = Receipts(
-                    meta = Meta(totalItems = 343), items = listOf(
-                        ReceiptDetail(
-                            id = "b16d63bf-39eb-45bd-bfbd-7631220ae3f2",
-                            name = "Slutty Pumpkin",
-                            cover = "https://i.imgur.com/ISxVZHA.png",
-                            favourite = true,
-                            favouriteAmount = 245,
-                            preparationTime = 900000,
-                        ),
-                        ReceiptDetail(
-                            id = "b16d63bf-39eb-45bd-bfbd-7631220ae3f2",
-                            name = "French toast",
-                            cover = "https://i.imgur.com/GNw5TTl.png",
-                            favourite = false,
-                            favouriteAmount = 124,
-                            preparationTime = 600000,
+            emit(
+                Resource.Success(
+                    data = Receipts(
+                        meta = Meta(totalItems = 343), items = listOf(
+                            ReceiptDetail(
+                                id = "b16d63bf-39eb-45bd-bfbd-7631220ae3f2",
+                                name = "Slutty Pumpkin",
+                                cover = "https://i.imgur.com/ISxVZHA.png",
+                                favourite = true,
+                                favouriteAmount = 245,
+                                preparationTime = 900000,
+                            ),
+                            ReceiptDetail(
+                                id = "b16d63bf-39eb-45bd-bfbd-7631220ae3f2",
+                                name = "French toast",
+                                cover = "https://i.imgur.com/GNw5TTl.png",
+                                favourite = false,
+                                favouriteAmount = 124,
+                                preparationTime = 600000,
+                            )
                         )
                     )
                 )
