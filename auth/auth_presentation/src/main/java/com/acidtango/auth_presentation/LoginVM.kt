cd2ebe07ec7769b7bca6c81a682bb602170a2596
Abraham Cárdenes <abraham@acidtango.com>
2022-09-15 12:16:35 +0100

    update: imports

--- auth/auth_presentation/src/main/java/com/acidtango/auth_presentation/LoginVM.kt
+++ auth/auth_presentation/src/main/java/com/acidtango/auth_presentation/LoginVM.kt
@@ -4,19 +4,10 @@ import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.stringPreferencesKey
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.acidtango.core.DataStoreDefaults
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
