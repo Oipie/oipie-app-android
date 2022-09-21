bd976096167541ec84d7df71640b83d8812cd5e6
Abraham Cárdenes <abraham@acidtango.com>
2022-09-21 15:20:24 +0100

    update: reduce duplications

--- auth/auth_presentation/src/main/java/com/acidtango/auth_presentation/viewModels/RegistrationVM.kt
+++ auth/auth_presentation/src/main/java/com/acidtango/auth_presentation/viewModels/RegistrationVM.kt
@@ -1,14 +1,10 @@
 package com.acidtango.auth_presentation.viewModels
 
-import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.acidtango.core.DataStoreInterface
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -35,5 +31,4 @@ class RegistrationVM
     fun onPasswordChange(password: String) {
         this.password = password
     }
-
 }
